354cfabe3da41d3b18b2fd777ce69d559fb64621
Andrew <andrewcchau@gmail.com>
2018-06-13 10:52:55 -0500

    Added empty properties template and main class

---
+++ src/Twitter_Main.java
@@ -0,0 +1,5 @@
+public class Twitter_Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Twitter_Publish.java
+++ src/Twitter_Publish.java
@@ -2,19 +2,20 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.Status;
 import twitter4j.TwitterFactory;
+import java.util.Scanner;
 
 public class Twitter_Publish {
     public static void main(String args[]){
         Twitter_Publish tp = new Twitter_Publish();
+        Scanner sc = new Scanner(System.in);
 
         try{
-            String message = "Test post 2: char limit not 280 ";
-            for(int i = message.length(); i < 279; i++){
-                message += "i";
-            }
+            String message = sc.nextLine();
             tp.postToTwitter(message);
         }catch(TwitterException te){
             te.printStackTrace();
+        }finally{
+            sc.close();
         }
     }
 
